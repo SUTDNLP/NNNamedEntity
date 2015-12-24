d395a5f19e0d4bb03a95b6646f7b60a08913919b
Jiesutd <jie_yang@mymail.sutd.edu.sg>
2015-12-24 11:48:57 +0800

    modify demo-entity.sh

--- demo-entity.sh
+++ demo-entity.sh
@@ -1,7 +1,4 @@
 cmake .
-wait
 make SparseTNNCRFMLLabeler
-wait
 ./SparseTNNCRFMLLabeler -l -train example/Entity.train -dev example/Entity.dev -option example/demo.option -word example/sena.emb -model example/demo.model
-wait
 ./SparseTNNCRFMLLabeler -test example/Entity.test -model example/demo.model -output example/Entity.test.output
